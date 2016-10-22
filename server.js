22fd2f7deaf4795d1b915509a0292e4691a95e95
Vrinda11 <vrindaceleb22@gmail.com>
2016-10-22 00:33:32 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ var counter=0;
 app.get('/counter'),function(req,res){
     counter=counter +1;
     res.send(counter.toString());
-    }
-    );
+    };
+
 
 app.get('/article-one',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
