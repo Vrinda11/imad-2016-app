29e41204387db8cc531022c93ad001edc48992de
Vrinda11 <vrindaceleb22@gmail.com>
2016-11-19 08:49:45 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,66 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title:'Change:VrindaB',
+    heading: 'Change',
+    date:'Sep 20,2016',
+  
+    content:  `
+                 <p>
+                 
+                 "To improve is to change; to be perfect is to change often."-Winston Churchill
+                    Change is scary,risky and sometimes dangerous.We all are afraid of it.All afraid to try it,try new things,try something profound and striking.Unless we have a sense of comfort we aren't confortable.It requires great courage,confidence and bravery to step out of our confort zone.The best secret to change is just to no think and do,
+                            trust your guts,trust your instincts and you are half way to success.If you believe your brain knows that it
+                          be done and so it works and acts accordingly.
+                      </p>
+                        <p>
+                         The first stepping stone to change is to believe in yourself and in creating the new out of the old.It it necessary to focus all our energies on finding solutions and fighting the problem.A strategy or a series of solution are required with proper implementation 
+                        and working out the plan requiresd.One need to be more imaginative,creative and expand one's horizons.One needs to be bolder in one's pursuits and there should be adequate time bound implementation of actions.
+                        </p>
+                        <p>
+                        "Action speaks louder than words".Thus with a serene,calm,stable,purified mind one should strive for one's goals and face adversity with determination and will power.                        
+                        </p> `
+
+    
+};
+
+var htmlTemplate = `<html>
+    <head>
+        <title>
+${title}
+        </title>
+        <meta name="viewport" content="width-device-width,initial-scale-1"/>
+           <link href="/ui/style.css" rel="stylesheet" />
+    <meta name="viewport" content='width-device-width,initial-scale-1'/>
+    
+   
+    </head>
+    <body>
+           <div class="container">
+        
+        <div  class="center">
+            <div>
+                <a href="/">Home</a>
+            </div>
+          <img style="-webkit-user-select: none;" src="https://www.mindtools.com/media/Resized-Images/Articles/Project_Management/25_IS_10761150_cglade_2x1.jpg" width="592" height="296">
+          <hr/>
+          <h3>
+          ${heading}
+          $</h3>
+          <div>
+          ${date}
+          </div>
+            <div>     
+            ${content}
+</div>
+    </div>
+    </div>
+    </body>
+</html>
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
